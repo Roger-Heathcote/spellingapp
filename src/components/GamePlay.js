39640022c3d05eeca5cfb7cd70477541456a05c8
Sampath Kumar <sampath.kumar@laureate.net>
2021-01-25 22:58:15 +0530

    Created basic basic game structue

--- src/components/GamePlay.js
+++ src/components/GamePlay.js
@@ -1,10 +1,17 @@
 import React,{useState, useEffect, useContext} from 'react';
 import SiteContext from "../SiteContext";
 function GamePlay({finished}){
+
+    function hideQuestion() {
+        console.log("I have hidden the word and enabled the text box")
+        setInputDisabled(false)
+        setDisplayWord(false)
+    }
+
     const [displayWord, setDisplayWord]= useState(true);
     useEffect(()=>{
         console.log("Running useEffect")
-        setTimeout(setDisplayWord,5000, false)
+        setTimeout(hideQuestion,5000)
 
 
     },[displayWord])
@@ -18,23 +25,27 @@ function GamePlay({finished}){
     const [words, updateWords] = useState([...state.currentLists[state.selectedListIndex].listWords])
     const [answer, updateAnswer] = useState('');
     const [displayCorrect, updateDisplayCorrect] = useState(false)
+    const [inputDisabled, setInputDisabled] = useState(true);
+
     let [currentWord, updateCurrentWord] = useState()
     
     const nextWord = () => {
-        updateCurrentWord(words[0])
-        console.log("Choosen word", currentWord)
-        updateWords(words.slice(1))
+      updateCurrentWord(words[0])
+      console.log("Choosen word", currentWord)
+      updateWords(words.slice(1))
     }
 
     if(!currentWord) nextWord()
 
     
     const answerHandler = (event) => {
+        updateAnswer(event.target.value)
         console.log("Checking answer", event.target.value)
         if(event.target.value === currentWord) {
           console.log("In the checker")
           updateDisplayCorrect(true)
           setTimeout(()=>{
+            // Display "Correct" for 3s then reset.
             updateDisplayCorrect(false)
             console.log("words length: ", words.length);
             if(words.length===0){
@@ -43,8 +54,16 @@ function GamePlay({finished}){
             }
             nextWord();
             setDisplayWord(true);
+            setInputDisabled(true);
+            updateAnswer("");
           }, 3000)
         }
+    }
+
+    const remindHandler = () => {
+      setDisplayWord(true);
+      setInputDisabled(true);
+      setTimeout(hideQuestion,5000);
     }
   
 	return (
@@ -55,7 +74,8 @@ function GamePlay({finished}){
             <div id="result">
                 {(displayCorrect) ? "Correct!" : ""}
             </div>
-            <input type="text" id="answer" onChange={(event) => answerHandler(event)}></input>
+            <input type="text" id="answer" onChange={(event) => answerHandler(event)} disabled={inputDisabled} value={answer}></input>
+            <button onClick={() =>remindHandler()}>Remind me</button>
             
         </>
     )
