193cdfaaecd8ea9af634f64a3a484c526f76abad
Sampath Kumar <sampath.kumar@laureate.net>
2021-02-17 23:40:50 +0530

    worked on age validation on AgeRange component

--- src/components/AgeRange.js
+++ src/components/AgeRange.js
@@ -2,28 +2,38 @@ import React,{useState} from 'react';
 
 
 function AgeRange(props){
-	
+  
+  const [showWarning, setWarning] = useState(false);
 	let [age] = useState(0);
-    const handleOnChange = (event) => {
-        age = event.target.value;
-    }
+  const handleOnChange = (event) => {
+        age = event.target.value; 
+  }
 	const keyHandler = (event) => {
 		if(event.key === "Enter") props.ageHandler(age);
-	}
+  }
+  const submitAge = () => {
+    if(age > 150){
+      setWarning(true);
+      setTimeout(() => setWarning(false), 2000);
+      return false;
+    }
+    props.ageHandler(age);
+  }
 	return (
-        <div> 
+        <div>
+           
+          <div className="warning">{(showWarning)?"Your age must be under 150!":""}</div>
 			<label>Enter your age:</label>
 			<input
-				type="text"
+				type="number"
 				name="age"
-				id="age"
+        id="age"
+        min="1"
+        max="150"
 				onChange={(event) => handleOnChange(event)}
 				onKeyDown={keyHandler}
 			/>
-			<button onClick={(event)=>{
-				console.log("I fired!")
-				props.ageHandler(age)}
-			}>Start</button>
+			<button onClick={()=>submitAge()}>Start</button>
 		</div>
     )
 }
