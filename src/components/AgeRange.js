89662a2f045a2063b0b700e4b084fd24c977f42b
Sampath Kumar <sampath.kumar@mail.waldenu.edu>
2021-05-29 16:12:40 +0530

    Worked on fixing - age validation

--- src/components/AgeRange.js
+++ src/components/AgeRange.js
@@ -1,48 +1,58 @@
-import React,{useState} from 'react';
+import React, { useState, useEffect } from 'react';
 
 
-function AgeRange(props){
-  
+function AgeRange(props) {
+
   const [showWarning, setWarning] = useState(false);
-	let age = 0;
-  const handleOnChange = (event) => {
-    age = event.target.value;
-    if(!isValidAge())
+  const [age, setAge] = useState(1);
+  /*
+  useEffect is used to check the value of age whenever there is a change in value of age.
+  if age < 0 or age > 150
+    setWarning(true)
+  else
+    setWarning(false)
+  */
+  useEffect(() => {
+    if (!isValidAge())
       setWarning(true);
     else
       setWarning(false);
-    console.log(age)
+  }, [age])
+  //set the entered value to age during onChange event
+  const handleOnChange = (event) => {
+    setAge(event.target.value);
   }
-	const keyHandler = (event) => {
-    console.log('key',age);
-    if(event.key === "Enter" && isValidAge())  
+  //when 'enter' key is pressed, validate the age in the input box
+  const keyHandler = (event) => {
+    if (event.key === "Enter" && isValidAge())
       props.ageHandler(age)
   }
+  //validating age - age should be above 0 and equal to or below 150
   const isValidAge = () => {
-    return (age > 150 || age <= 0)? false: true;
+    return (age > 150 || age <= 0) ? false : true;
   }
-  const submitAge = () => { 
-    if(isValidAge())
-       props.ageHandler(age);
-      
+  //when submit button is clicked, validate the age and pass to ageHandler function
+  const submitAge = () => {
+    if (isValidAge())
+      props.ageHandler(age);
   }
-	return (
-        <div>
-           
-          <div className="warning">{(showWarning)?"Your age must be under 150 and above 0!":""}</div>
-			<label>Enter your age:</label>
-			<input
-				type="number"
-				name="age"
+  return (
+    <div>
+
+      <div className="warning">{(showWarning) ? "Your age must be under 150 and above 0!" : ""}</div>
+      <label>Enter your age:</label>
+      <input
+        type="number"
+        name="age"
         id="age"
         min="1"
         max="150"
-				onChange={(event) => handleOnChange(event)}
-				onKeyDown={keyHandler}
-			/>
-			<button onClick={()=>submitAge()}>Start</button>
-		</div>
-    )
+        onKeyDown={keyHandler}
+        onChange={(event) => handleOnChange(event)}
+      />
+      <button onClick={() => submitAge()}>Start</button>
+    </div>
+  )
 }
 
 export default AgeRange
