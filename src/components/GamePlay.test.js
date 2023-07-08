42cb862b122ce857c303325513852ca5dda94901
Sampath Kumar Nagarajan <sampathkumar.n01@ad.infosys.com>
2023-07-08 20:35:33 +0530

    Removed unused imports, revert start command changes

--- src/components/GamePlay.test.js
+++ src/components/GamePlay.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render, act, waitFor, screen} from '@testing-library/react';
+import {render, act, waitFor} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import SiteContext from '../SiteContext';
 import GamePlay from './GamePlay.js';
